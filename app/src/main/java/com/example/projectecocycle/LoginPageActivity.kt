c9f6552877716f2d034c13523e2a7dfe37755fb6
DP5-WilbertNeilsonSachio-048 <willbert1206@gmail.com>
2024-11-04 00:27:23 +0700

    menambah intent dari login page ke konfirmation page, dan mengubah penamaan beberapa file

--- app/src/main/java/com/example/projectecocycle/LoginPageActivity.kt
+++ app/src/main/java/com/example/projectecocycle/LoginPageActivity.kt
@@ -1,20 +1,24 @@
 package com.example.projectecocycle
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginPage : AppCompatActivity() {
+class LoginPageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val nextButton = findViewById<Button>(R.id.tv_tombolselanjutnya)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, ConfirmationRegisterPageActivity::class.java)
+            startActivity(intent)
         }
     }
 }
