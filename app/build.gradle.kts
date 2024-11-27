b730eb4bbd5494d5a7c25567fe7ecf2a0187125b
DP5-WilbertNeilsonSachio-048 <willbert1206@gmail.com>
2024-11-27 23:51:34 +0700

    logout image gmail done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.material.v190)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
